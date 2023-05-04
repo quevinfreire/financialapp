4afc7a15c599886e5dc90746cff857d41def65e9
Quevin Freire <quevinfreire93@gmail.com>
2023-05-04 02:32:10 -0400

    Created project

---
+++ src/test/java/com/quevinfreire/financialapp/FinancialappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quevinfreire.financialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
